04cd01d4fe19ef2c6b3f6ebe76f0f4545ad8c8a7
Jack Wearden <jack@jackwearden.co.uk>
2014-02-28 23:57:18 +0000

    Initial version

---
+++ simple.py
@@ -0,0 +1,32 @@
+import pygame, math, sys
+from pygame.locals import FULLSCREEN, K_UP, K_DOWN, K_LEFT, K_RIGHT, K_ESCAPE
+
+
+def main():
+    screen      = pygame.display.set_mode((1024, 768), FULLSCREEN)
+    image       = pygame.image.load('wow.png')
+    clock       = pygame.time.Clock()
+
+    black       = (0,0,0)
+    play_game   = True
+    x_position  = 50
+    y_position  = 50
+
+    while play_game:
+        clock.tick(60)
+        screen.fill(black)
+
+        key_pressed = pygame.key.get_pressed()
+
+        if key_pressed[K_ESCAPE]:
+            play_game = False
+        if key_pressed[K_RIGHT]:
+            x_position += 5
+        
+        screen.blit(image, (x_position, y_position))
+        pygame.display.flip()
+        pygame.event.pump()
+
+    pygame.quit()
+
+main()
